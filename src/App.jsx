2986e27595de0941633028e36de71fe2baa378e7
Sanjaykanwasi <sanjaykanwasi3@gmail.com>
2025-05-21 13:14:58 +0530

    mid_part

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
+import { useGSAP } from "@gsap/react";
+import "remixicon/fonts/remixicon.css";
+import gsap from "gsap";
 
 function App() {
-  return <div>App</div>;
+  let [showContent, setShowContent] = useState(false);
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
+
+    tl.to(".vi-mask-group", {
+      rotate: 10,
+      duration: 2,
+      ease: "Power4.easeInOut",
+      transformOrigin: "50% 50%",
+    }).to(".vi-mask-group", {
+      scale: 10,
+      duration: 2,
+      delay: -1.8,
+      ease: "Expo.easeInOut",
+      transformOrigin: "50% 50%",
+      opacity: 0,
+
+      onUpdate: function () {
+        if (this.progress() >= 0.9) {
+          document.querySelector(".svg").remove();
+          setShowContent(true);
+          this.kill();
+        }
+      },
+    });
+  });
+
+  return (
+    <>
+      <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
+        <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
+          <defs>
+            <mask id="viMask">
+              <rect width="100%" height="100%" fill="black" />
+              <g className="vi-mask-group">
+                <text
+                  x="50%"
+                  y="50%"
+                  fontSize="250"
+                  textAnchor="middle"
+                  fill="white"
+                  dominantBaseline="middle"
+                  fontFamily="Arial Black"
+                >
+                  VI
+                </text>
+              </g>
+            </mask>
+          </defs>
+          <image
+            href="./bg.png"
+            width="100%"
+            height="100%"
+            preserveAspectRatio="xMidYMid slice"
+            mask="url(#viMask)"
+          />
+        </svg>
+      </div>
+      {showContent && (
+        <div className="main w-full">
+          <div className="landing w-full bg-black h-screen">
+            <div className="navbar absolute top-0 left-0 z-[10] w-full py-10 px-10">
+              <div className="logo flex items-center gap-5">
+                <div className="lines flex flex-col gap-1">
+                  <div className="line w-10 h-[6px] bg-white"></div>
+                  <div className="line w-10 h-[6px] bg-white"></div>
+                  <div className="line w-10 h-[6px] bg-white"></div>
+                </div>
+                <h3 className="text-5xl -mt-[5px] leading-none text-orange-400">
+                  Rockstar
+                </h3>
+              </div>
+            </div>
+            <div className="imagesdiv relative overflow-hidden w-full h-screen">
+              <img
+                className="absolute top-0 left-0 w-full h-full object-cover"
+                src="./sky.png"
+                alt=""
+              />
+              <img
+                className="absolute top-0 left-0 w-full h-full object-cover"
+                src="./bg.png"
+                alt=""
+              />
+              <img
+                className="absolute -bottom-[60%] left-1/2 -translate-x-1/2"
+                src="./girl.png"
+                alt=""
+              />
+            </div>
+            <div className="btmbar absolute bottom-0 left-0 w-full py-10 px-10 bg-gradient-to-t from-black to-transparent">
+              <div className="flex gap-4 "></div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
